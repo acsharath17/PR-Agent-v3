45ae8be581845d51af736cf2e292c15605e630a5
Sharath Chikkanna <sharathchikkanna@mac.home>
2025-06-08 00:03:16 +0100

    n

--- worker.py
+++ worker.py
@@ -1,14 +1,15 @@
 import os
 import ssl
 from redis import Redis
-from rq import Worker, Queue, Connection
+from rq import Worker, Queue
+from rq.connections import Connection
 
 listen = ['default']
-redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDIS_URL')
 
 conn = Redis.from_url(
     redis_url,
-    ssl_cert_reqs=ssl.CERT_NONE if redis_url.startswith("rediss://") else None
+    ssl_cert_reqs=ssl.CERT_NONE if redis_url and redis_url.startswith("rediss://") else None
 )
 
 if __name__ == '__main__':
